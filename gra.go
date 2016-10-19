4a5d730c766882da2ec03f4cf6928ba777830f4f
Franciszek Boehlke <franio.kropka.b@gmail.com>
2016-10-20 01:32:00 +0200

    Bonuses added (double guns)

--- gra.go
+++ gra.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"time"
+	"math/rand"
 )
 
 type event int
@@ -25,6 +26,10 @@ type projectile struct {
 	x, y int
 }
 
+type bonus struct {
+	x, y int
+}
+
 func main() {
 	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
 	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
@@ -40,8 +45,8 @@ func main() {
 				if arrowState == 0 {
 					switch b[0] {
 						case 27: arrowState = 1
-						case 32: inputEvents <- fireEvent
-						case 4: inputEvents <- quitEvent
+						case ' ': inputEvents <- fireEvent
+						case 4, 'q': inputEvents <- quitEvent
 					}
 				} else if arrowState == 1 {
 					switch b[0] {
@@ -69,8 +74,10 @@ func main() {
 	moveDirection := 1
 	xpos, ypos := 10, displayHeight-1
 	minx, maxx := 3, displayWidth - 4
+	hasDoubleGun := false
 	
 	projectiles := make([]projectile, 0)
+	bonuses := make([]bonus, 0)
 	
 	ticker := time.Tick(20 * time.Millisecond)
 	
@@ -79,6 +86,11 @@ func main() {
 	for iy := range displayContent {
 		displayContent[iy] = make([]string, displayWidth)
 	}
+
+	fmt.Println()
+	fmt.Println("\x1b[45m                                                                    \x1b[0m")
+	fmt.Println("\x1b[45m  \x1b[0;1m                    Welcome here, brave pilot!                  \x1b[45m  \x1b[0m")
+	fmt.Println("\x1b[45m                                                                    \x1b[0m")
 	
 	for {
 		eventReading: for {
@@ -90,7 +102,12 @@ func main() {
 				case arrowRigth:
 					moveDirection = 1
 				case fireEvent:
-					projectiles = append(projectiles, projectile{xpos, ypos-1})
+					if hasDoubleGun {
+						projectiles = append(projectiles, projectile{xpos-1, ypos})
+						projectiles = append(projectiles, projectile{xpos+1, ypos})
+					} else {
+						projectiles = append(projectiles, projectile{xpos, ypos-1})
+					}
 				case quitEvent:
 					fmt.Println("\x1b[1;45m                                                                    \x1b[0m")
 					fmt.Println("\x1b[1;43m                                                                    \x1b[0m")
@@ -117,6 +134,22 @@ func main() {
 		}
 		projectiles = leftProjectiles
 		
+		if rand.Intn(100) == 0 {
+			bonuses = append(bonuses, bonus{rand.Intn(displayWidth), -1})
+		}
+		leftBonuses := make([]bonus, 0, len(bonuses)+5)
+		for _, bonus := range bonuses {
+			bonus.y += 1
+			if bonus.y < displayHeight {
+				if bonus.y == ypos && bonus.x - xpos <= 1 && xpos - bonus.x <= 1 {
+					hasDoubleGun = true
+				} else {
+					leftBonuses = append(leftBonuses, bonus)
+				}
+			}
+		}
+		bonuses = leftBonuses
+		
 		// clear display buffer (fill with spaces)
 		for iy := 0; iy < displayHeight; iy++ {
 			for ix := 0; ix < displayWidth; ix++ {
@@ -125,7 +158,11 @@ func main() {
 		}
 		
 		
-		// draw on the buffer
+		// draw on the buffer:
+		// draw bonuses
+		for _, bonus := range bonuses {
+			displayContent[bonus.y][bonus.x] = "\x1b[1;32m$\x1b[0m"
+		}
 		
 		// draw projectiles
 		for _, projectile := range projectiles {
@@ -140,7 +177,6 @@ func main() {
 		
 		
 		// print buffer to terminal
-		fmt.Print("\x1b[H\x1b[J")
 		for iy := 0; iy < displayHeight; iy++ {
 			for ix := 0; ix < displayWidth; ix++ {
 				fmt.Print(displayContent[iy][ix])
@@ -150,5 +186,8 @@ func main() {
 		fmt.Println()
 		
 		<-ticker  // zaczekaj na kolejny tick
+		
+		// clear terminal before next frame
+		fmt.Printf("\x1b[%dA\x1b[G", displayHeight+1)
 	}
 }
